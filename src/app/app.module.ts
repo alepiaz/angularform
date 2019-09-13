f05299dcd44b48d85211dbfe8e41561dac3923f3
Alessio <alexsquare9511@gmail.com>
2019-09-13 17:53:58 +0200

    removed user error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,8 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule,
+      AppRoutingModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor(
+        public firstname: string,
+        public lastname: string,
+        public email: string,
+        public cellphone: string,
+        public birth: string,
+        public address: string,
+        public civnumber: string,
+        public city: string,
+        public zipcode: string
+    ){}
+}
