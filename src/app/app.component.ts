b43abe8d3e39d0e023e377355f3e6170cbd92c59
Alessio <alexsquare9511@gmail.com>
2019-09-17 16:38:28 +0200

    added validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,19 @@ import {Component, NgModule} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  max: number;
+
+  checkDate() {
+    const t = new Date();
+    const m = t.getMonth();
+    const d = t.getDay();
+    const y = t.getFullYear() - 18;
+    const today = new Date(y, m, d).toISOString().slice(0, 10);
+
+    return today;
+  }
+
 }
 
 
